d2ebb6e326e6e86daf073ef47d83d79d5c699b5c
Anamul-09 <mdanamulislam09@gmail.com>
2022-09-23 00:06:03 +0600

    updatebd

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$db= new mysqli('localhost','root','','project_1_fos')or die("Could not connect to mysql".mysqli_error($db));
+$db= new mysqli('localhost','u318076185_anamul','8l|Pfv:wQE','u318076185_project1')or die("Could not connect to mysql".mysqli_error($db));
